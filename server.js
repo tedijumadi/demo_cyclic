15abfb802ca636d599d3709d594d12868eea5578
tedijumadi <tedijumadi368@gmail.com>
2023-09-06 15:08:35 +0700

    what: server created

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.get('/add', (req, res) => {
+    res.send('Hellow World!');
+});
+
+app.get('/add', (req, res) => {
+    res.send('New recornd added.');
+});
+
+//API to show demo data
+app.get('/demo',(req, res) =>
+    res.json([
+        {
+            id:'001',
+            name:'Smith',
+            email:'smith@gmail.com',
+
+        },
+        {
+            id:'002',
+            name:'Sam',
+            email:'sam@gmail.com',
+
+        },
+        {
+            id:'003',
+            name:'Lily',
+            email:'lily@gmail.com',
+
+        }
+    ])
+)
+
+
+
+const HTTP_PORT = process.env.PORT || 8080;
+app.listen(HPPS_PORT, () =>{
+    console.log(`Server is listening at port ${HTTP_PORT}`);
+});
